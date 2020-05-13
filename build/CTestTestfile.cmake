c1cf898dec78c6daeb1f20ed9e9c284732d7c574
Emily Owen McDougall <eom1002@lolcat.sr.unh.edu>
2020-05-13 02:58:04 +0000

    here is the code for my project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/eom1002/Seshat
+# Build directory: /home/eom1002/Seshat/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("_deps/xtl-build")
+subdirs("_deps/xtensor-build")
---
+++ build/_deps/xtensor-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eom1002/Seshat/build/_deps/xtensor-src
+# Build directory: /home/eom1002/Seshat/build/_deps/xtensor-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
